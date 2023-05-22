c9e3ddace192d2018b63d2bb633736a622a70b17
bbadah <bilal.badah@capgemini.com>
2023-05-22 17:25:24 +0100

    20. Configuring DbContext and using Dependency Injection from Startup.cs

--- StudentAdminPortal.API/Program.cs
+++ StudentAdminPortal.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using StudentAdminPortal.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Inject DbConetxt into the service container
+builder.Services.AddDbContext<StudentAdminDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("StudentAdminPortalDb")));
+
 
 var app = builder.Build();
 
