0586314c665370f3b1db57f0beece6af161160af
bbadah <bilal.badah@capgemini.com>
2023-05-25 15:02:11 +0100

    23. Creating student repository - Repository pattern

---
+++ StudentAdminPortal.API/Repositories/SqlStudentRepository.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StudentAdminPortal.API.Data;
+using StudentAdminPortal.API.Models;
+
+namespace StudentAdminPortal.API.Repositories
+{
+    public class SqlStudentRepository : IStudentRepository
+    {
+        private readonly StudentAdminDbContext _context;
+
+        public SqlStudentRepository(StudentAdminDbContext context)
+        {
+            this._context = context;
+        }
+        public List<Student> GetStudents()
+        {
+            return _context.Student.ToList();
+        }
+    }
+}
